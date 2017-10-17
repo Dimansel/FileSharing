9277a51ac8756d358cac38a392062938449e4b7d
88maxwell <maxwell.soltyk@mail.ru>
2017-10-18 01:39:55 +0300

    add breadcrumb and his switcher

--- app/js/main.js
+++ app/js/main.js
@@ -3,15 +3,26 @@ function goTo(path){
   formData.append("path", path);
   var request = new XMLHttpRequest();
   request.open("POST", "/files");
-  request.send({"path":path});
-  console.log(path);
+  request.send(formData);
   request.onload = function(){
     data=JSON.parse(this.responseText);
     if (data["error"]){
       alert("Directory not available");
     }else{
-      var nav = document.getElementsById("nav-files");
+      var nav = document.getElementById("nav-files");
       nav.innerHTML = data["html"];
     }
+  }
+}
+
+function switchBreadcrumb(){
+  var bcList = document.getElementsByClassName("l-bcList")[0]
+  var bcInput = document.getElementsByClassName("l-bcInput")[0]
+  if (bcList.className == "l-bcList is-hidden"){
+    bcList.setAttribute("class", "l-bcList");
+    bcInput.setAttribute("class", "l-bcInput is-hidden");
+  }else{
+    bcList.setAttribute("class", "l-bcList is-hidden");
+    bcInput.setAttribute("class", "l-bcInput");
   }
 }
