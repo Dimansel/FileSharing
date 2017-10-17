f36a060f06ceab0d19c3f48a92d0c01ed390d298
88maxwell <maxwell.soltyk@mail.ru>
2017-10-17 22:45:42 +0300

    add JS, and pushing at dir. or files without reboot page

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,17 @@
-//HELLO WORLD
+function goTo(path){
+  var formData = new FormData();
+  formData.append("path", path);
+  var request = new XMLHttpRequest();
+  request.open("POST", "/files");
+  request.send({"path":path});
+  console.log(path);
+  request.onload = function(){
+    data=JSON.parse(this.responseText);
+    if (data["error"]){
+      alert("Directory not available");
+    }else{
+      var nav = document.getElementsById("nav-files");
+      nav.innerHTML = data["html"];
+    }
+  }
+}
